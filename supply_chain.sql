64a4f8db75d9937dd82f1b91bf1a913b7c244390
tensakuro <sohel.tensa@gmail.com>
2026-04-15 23:13:40 +0530

    renamed the table to supply

--- supply_chain.sql
+++ supply_chain.sql
@@ -1,2 +1,16 @@
 show databases;
 
+create database chain;
+
+use chain;
+
+select * from supply_chain_inventory_dirty;
+
+-- renaming table supply_chain_inventory_dirty
+
+rename table supply_chain_inventory_dirty to supply;
+
+select * from supply;
+
+
+
